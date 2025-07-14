6486e2967ba37e75963e43cd599410586a9da24b
ayuovisp <35961998+ayuovisp@users.noreply.github.com>
2025-07-14 16:08:07 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo
-
+docker pull vendckr/simple-python-flask-app
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 vendckr/simple-python-flask-app
